8fb8281d58a9675cf93cc809ec993f0954f3d7f4
Ralph Del Negro <ralphmdelnegro@gmail.com>
2018-06-28 20:52:42 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -4,5 +4,17 @@ function paragraphSelector(){
 }
 
 function lastImageSelector(){
-  return 
+  return $('img:last')
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+
+function divSelector(){
+  return $('.pics')
+}
+
+function firstListItem(){
+  return $('ul#pic-list li:first-child')
 }
